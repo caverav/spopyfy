93c59b21c3b578c91472b51dcf87114fd80e8987
Ricardo <ricardo.olalquiaga@sansano.usm.cl>
2022-05-25 16:10:42 -0400

    todo added

--- include/insert_db.php
+++ include/insert_db.php
@@ -1,11 +1,13 @@
 <?php
     require("../creds.php");
-    session_start();
+    //session_start();
 
     $email = $_POST["email"];
     $password = $_POST["password"];
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
+
+    //TODO: Insertar contraseña hasheada
 
     $sql_statement = "INSERT INTO personas(email, password, nombre, apellido) VALUES ($1, $2, $3, $4);";
     $result = pg_query_params($db_conn, $sql_statement, array($email, $password, $nombre, $apellido));
