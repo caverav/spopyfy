d22c957cdc42532a0251d94fd1621d5d7e6d2f98
Ricardo <ricardo.olalquiaga@sansano.usm.cl>
2022-05-25 15:36:52 -0400

    añadido atributos en register

--- include/insert_db.php
+++ include/insert_db.php
@@ -4,8 +4,10 @@
 
     $email = $_POST["email"];
     $password = $_POST["password"];
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
 
-    $sql_statement = "INSERT INTO personas(email, password) VALUES ($1, $2);";
-    $result = pg_query_params($db_conn, $sql_statement, array($email, $password));
-    header("Location: ../html/menu.html?success");
+    $sql_statement = "INSERT INTO personas(email, password, nombre, apellido) VALUES ($1, $2, $3, $4);";
+    $result = pg_query_params($db_conn, $sql_statement, array($email, $password, $nombre, $apellido));
+    header("Location: ../html/login.html?success");
 ?>
