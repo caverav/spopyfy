ca5aae0476eedffeaaff9c78192f57f9448639d2
Camilo <camilo.verav@sansano.usm.cl>
2022-05-28 02:31:29 -0400

    added burger menu & fixed profile

--- include/session_verification.php
+++ include/session_verification.php
@@ -4,10 +4,9 @@ session_start();
 
 //TODO: SOLO DIRIGE A USUARIO NORMAL, NO ARTISTA
 
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-    header("Location: ../html/index_usuario.html");
-} else {
+// if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+//     header("Location: ../html/index_usuario.html");
+// }
     
-}
 
 ?>
--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,20 @@
 // Grab HTML Elements
-const btn = document.querySelector("button.mobile-menu-button");
+const btn = document.querySelector(".burger");
 const menu = document.querySelector(".mobile-menu");
+const cross = document.querySelector(".cross");
+const user = document.querySelector(".user");
+const profile = document.querySelector(".profile");
+
 
 // Add Event Listeners
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
+});
+
+cross.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
+
+user.addEventListener("click", () => {
+    profile.classList.toggle("hidden");
 });
