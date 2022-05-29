bdf842f53ec031f2fca000fbad0be600e9469ffa
Camilo <camilo.verav@sansano.usm.cl>
2022-05-29 16:38:17 -0400

    fixed alert redirection

--- include/alert.php
+++ include/alert.php
@@ -1,7 +1,7 @@
 <?php
 $successHTML = '
-<div class="flex justify-center">
-    <div class="inline-flex items-center px-6 py-5 mx-auto mb-3 t-4 text-base text-green-700 bg-green-100 rounded-lg m"
+<div class="absolute flex justify-center mx-[35%] mt-2">
+    <div class="inline-flex items-center px-6 py-5 mx-auto mb-3 text-base text-green-700 bg-green-100 rounded-lg t-4 m"
         role="alert">
         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle"
             class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
@@ -15,7 +15,7 @@ $successHTML = '
 
 $errorHTML = '
 <div class="flex justify-center">
-    <div class="inline-flex items-center px-6 py-5 mx-auto mb-3 t-4 text-base text-red-700 bg-red-200 rounded-lg" role="alert">
+    <div class="inline-flex items-center px-6 py-5 mx-auto mb-3 text-base text-red-700 bg-red-200 rounded-lg t-4" role="alert">
         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path>
         </svg>
--- include/insert_db.php
+++ include/insert_db.php
@@ -1,18 +1,15 @@
 <?php
     require dirname(__DIR__).'/db_config.php';
-    // echo root path
-    // echo dirname(__FILE__);
 
     $email = $_POST["email"];
     $password = $_POST["password"];
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
 
-    
-    //TODO: Insertar contraseña hasheada
+
     $password_hashed = password_hash($password, PASSWORD_DEFAULT);
     $sql_statement = "INSERT INTO personas(email, password, nombre, apellido) VALUES ($1, $2, $3, $4);";
     $result = pg_query_params($dbconn, $sql_statement, array($email, $password_hashed, $nombre, $apellido));
 
-    header("Location: ../login.html?success");
+    header("Location: ../html/login.html?success");
 ?>
