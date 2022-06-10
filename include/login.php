14bee7a5fa2682fc6c83c3849664cda2fa8b7858
Ricardo <ricardo.olalquiaga@sansano.usm.cl>
2022-06-10 00:28:01 -0400

    agregado la id como var de sesion

--- include/login.php
+++ include/login.php
@@ -5,11 +5,11 @@
     $password = $_POST["password"];
 
     // Obtenemos la contraseña hasheada del usuario de la BD.
-    $sql_statement_users = "SELECT password, nombre, apellido, suscripcion_activa FROM usuarios WHERE email = $1;";
+    $sql_statement_users = "SELECT password, nombre, apellido, id, suscripcion_activa FROM usuarios WHERE email = $1;";
     $result_users = pg_query_params($dbconn, $sql_statement_users, array($email));
 
     //Para artistas
-    $sql_statement_artists = "SELECT password, nombre, apellido, nombre_artistico, verificado FROM artistas WHERE email = $1;";
+    $sql_statement_artists = "SELECT password, nombre, apellido, id, nombre_artistico, verificado FROM artistas WHERE email = $1;";
     $result_artists = pg_query_params($dbconn, $sql_statement_artists, array($email));
 
     // si no encuentra resultados
@@ -27,9 +27,10 @@
         $_SESSION["apellido"] = $row[2];
         $_SESSION['loggedin'] = true;
         $_SESSION['tipo'] = pg_num_rows($result_users) != 0 ? "usuario" : "artista";
-        $_SESSION['suscripcion_activa'] = $row[3] == 't' ? 'Activa' : 'Inactiva';
-        $_SESSION['nombre_artistico'] = pg_num_rows($result_users) == 0 ? $row[3] : "";
-        $_SESSION['verificado'] = pg_num_rows($result_users) == 0 ? $row[4] : "";
+        $_SESSION['suscripcion_activa'] = $row[4] == 't' ? 'Activa' : 'Inactiva';
+        $_SESSION['nombre_artistico'] = pg_num_rows($result_users) == 0 ? $row[4] : "";
+        $_SESSION['verificado'] = pg_num_rows($result_users) == 0 ? $row[5] : "";
+        $_SESSION['id'] = $row[3];
         header("Location: ../index.html");
     }
     else {
