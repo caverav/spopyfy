7dcb078b05304aad9bd5ec40cc3947c4a0afcbe8
Camilo <camilo.verav@sansano.usm.cl>
2022-06-10 14:07:51 -0400

    CRUD canciones

--- include/editar_cancion.php
+++ include/editar_cancion.php
@@ -3,7 +3,24 @@
 <?php include '../db_config.php'; ?>
 <?php
 //TODO: dejarlo bonito y asegurarse de que se pueda editar la cancion
-if (isset($_POST['id'])) {
+ini_set('display_errors', 1);
+
+if (isset($_POST['action']) && $_POST['action'] == 'delete') {
+    $id = $_POST['id'];
+    $sql = "DELETE FROM album_tiene_cancion WHERE id_cancion = $1";
+    $result = pg_query_params($dbconn, $sql, array($id));
+    $sql = "DELETE FROM artista_compuso_cancion WHERE id_cancion = $1";
+    $result = pg_query_params($dbconn, $sql, array($id));
+    $sql = "DELETE FROM canciones WHERE id = $1";
+    $result = pg_query_params($dbconn, $sql, array($id));
+    if ($result) {
+        header('Location: ../html/crud_canciones.html?success');
+    } else {
+        header('Location: ../html/crud_canciones.html?error');
+    }
+}
+
+if (isset($_POST['action']) && $_POST['action'] == 'update') {
     $id = $_POST['id'];
     $nombre = $_POST['nombre'];
     $letra = $_POST['letra'];
@@ -24,7 +41,7 @@ if (isset($_POST['id'])) {
         $cancion = pg_fetch_assoc($result);
         echo   '
             <div class="flex justify-center mt-4">
-                <form action="crud_canciones.html" method="POST">
+                <form action="editar_cancion.php" method="POST">
                     <div class="flex flex-wrap mb-6 -mx-3">
                         <div class="w-full px-3 mb-6 md:mb-0">
                             <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="name">
@@ -45,15 +62,18 @@ if (isset($_POST['id'])) {
                             <input class="block w-full px-4 py-3 leading-tight text-gray-700 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-date"
                                 type="date" name="fecha_composicion" value="'.htmlspecialchars($cancion['fecha_composicion']).'" required>
                         </div>
-                        <input type="hidden" name="ADD_SONG" value="true">
+                        <input type="hidden" name="id" value="'.$id.'">
                     </div>';
         $sql = "SELECT * FROM album WHERE id IN (SELECT id_album FROM album_tiene_cancion WHERE id_cancion = $id)";
         $result = pg_query_params($dbconn, $sql, array());
         while ($album = pg_fetch_assoc($result)) {
             echo '<input type="checkbox" name="albumes[]" value="'.htmlspecialchars($album['id']).'" checked>'.htmlspecialchars($album['nombre']).'<br>';
         }
-        echo '<div class="flex justify-center">
-            <button class="px-4 py-2 font-bold text-white bg-pink-500 rounded hover:bg-pink-700 focus:outline-none focus:shadow-outline" type="submit">
+        echo '<div class="flex justify-around">
+            <button class="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-pink-700 focus:outline-none focus:shadow-outline" type="submit" name="action" value="delete">
+                Eliminar canción
+            </button>
+            <button class="px-4 py-2 font-bold text-white bg-pink-500 rounded hover:bg-pink-700 focus:outline-none focus:shadow-outline" type="submit" name="action" value="update">
                 Agregar canción
             </button>
         </div>';
